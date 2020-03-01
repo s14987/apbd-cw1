4194160b0736e83260fbcd4258e66663926a7395
s14987 <s14987@pja.edu.pl>
2020-03-01 17:56:23 +0100

    dodałem HtpClient()

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Cwiczenia1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+            
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
